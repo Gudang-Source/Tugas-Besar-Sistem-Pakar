912e30d60f55585bb747661895768889407e79db
arjunyudafirwanda <arjunyudafirwanda@gmail.com>
2020-05-19 22:17:35 +0700

    1174008 Arjun

---
+++ resources/views/layouts/includes/tovbar.blade.php
@@ -0,0 +1,21 @@
+<header id="header">
+    <div class="header-top">
+  </div>
+  <div class="container main-menu">
+      <div class="row align-items-center justify-content-between d-flex">
+          <a href="index.html"><img src="img/logo.png" alt="" title="" /></a>
+          <nav id="nav-menu-container">
+              <ul class="nav-menu">
+                <li class="menu-active"><a href="index.html">Home</a></li>
+                <li><a href="about.html">Jenis Kendaraan</a></li>
+                <li><a href="service.html">Kategori</a></li>
+                <li><a href="gallery.html">Kendaraan</a></li>
+                <li><a href="elements.html">Rental Kendaraan</a></li>
+                <li><a href="contact.html">Rules</a></li>
+                <li><a href="contact.html">User</a></li>
+
+              </ul>
+          </nav><!-- #nav-menu-container -->
+      </div>
+  </div>
+</header><!-- #header -->
