d3123d3744d183dda31c81f7a2989d40dd12fdd3
Dwi Yulianingsih <dwiyulianingsih21@gmail.com>
2020-06-05 01:17:27 +0700

    header index

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,5 +1,20 @@
 @extends('layouts/master')
-
+<!-- start banner Area -->
+<section class="banner-area relative about-banner" id="home">	
+				<div class="overlay overlay-bg"></div>
+				<div class="container">				
+					<div class="row d-flex align-items-center justify-content-center">
+						<div class="about-content col-lg-12">
+							<h1 class="text-white">
+								Home			
+							</h1>	
+							<p class="text-white link-nav"><a href="index.html"></a></p>
+						</div>	
+					</div>
+				</div>
+			</section>
+			<!-- End banner Area -->
+            
 @section('content')
 
 @include('layouts/includes/tovbar')
